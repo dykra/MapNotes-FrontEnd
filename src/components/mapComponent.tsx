ed07830b5c3d6c3a243310a9e27d937b727e9b99
justynamaciag <justynamaciag96@gmail.com>
2018-04-09 23:11:03 +0200

    Appearance Change

--- src/components/mapComponent.tsx
+++ src/components/mapComponent.tsx
@@ -1,7 +1,7 @@
 
 import * as React from 'react';
 
-const logo = require('../images/1013.jpg');
+// const logo = require('../images/1013.jpg');
 
 export default class HelloWorld extends React.Component {
 
@@ -12,10 +12,10 @@ export default class HelloWorld extends React.Component {
                     {/*name = "Update"*/}
                     {/*onClick = { e => this.handleOnClick(e) }*/}
                 {/*>Update</button>*/}
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">MapNotes</h1>
-                </header>
+                {/*<header className="App-header">*/}
+                    {/*/!*<img src={logo} className="App-logo" alt="logo" />*!/*/}
+                    {/*/!*<h1 className="App-title">MapNotes</h1>*!/*/}
+                {/*</header>*/}
                 <p className="App-intro">
                          App to handle real estates <code>src/App.tsx</code> and save to reload.
                 </p>
