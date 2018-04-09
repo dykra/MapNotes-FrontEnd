225c815cba39e49d2045b909949b96cc2726e082
dykra <asiapalewicz@gmail.com>
2018-04-09 21:21:57 +0200

    componends added

---
+++ src/components/mapComponent.tsx
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+
+const logo = require('../images/1013.jpg');
+
+export default class HelloWorld extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                {/*<button*/}
+                    {/*name = "Update"*/}
+                    {/*onClick = { e => this.handleOnClick(e) }*/}
+                {/*>Update</button>*/}
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">MapNotes</h1>
+                </header>
+                <p className="App-intro">
+                         App to handle real estates <code>src/App.tsx</code> and save to reload.
+                </p>
+            </div>
+    );
+    }
+}
