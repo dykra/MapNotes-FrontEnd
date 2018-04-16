971476f67dad305dc2ddf51292f7b66c33c9599d
dykra <asiapalewicz@gmail.com>
2018-04-16 10:37:06 +0200

    Map1 name changed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import GoogleMapReact from 'google-map-react'
 
 
-export default class Map1 extends Component {
+export default class Map extends Component {
     static defaultProps = {
         center: { lat: 40.7446790, lng: -73.9485420 },
         zoom: 11
