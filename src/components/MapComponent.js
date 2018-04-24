75055e0fd1fc228c4478d9bc749ac39aedb6e373
Anna Bukowska <woda33@vp.pl>
2018-04-24 08:28:18 +0200

    Two different notes for marker[Odys1-7]

---
+++ src/components/MapComponent.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { compose, withProps } from "recompose";
+import { withGoogleMap, withScriptjs, GoogleMap } from 'react-google-maps';
+import InfoWindowMap from "./MarkerInfoWindow";
+
+
+const MapComponent = compose(
+    withScriptjs,
+    withGoogleMap,
+)
+(props => {
+    return (
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{lat: -34.397, lng: 150.644}}
+            onClick={props.onMapClick}
+        >
+            {props.markers.map((marker, index) =>
+                <InfoWindowMap
+                    lat={marker.position.lat()}
+                    lng={marker.position.lng()}
+                    index={index}
+                    key={marker.position}
+                />
+            )}
+        </GoogleMap>
+    )
+});
+
+export default class MapContainer extends React.Component {
+
+    constructor () {
+        super();
+        this.state = {
+            markers : []
+        };
+    }
+
+    handleMapClick(event) {
+
+        this.setState(prevState => ({
+            markers: [...prevState.markers,  { position: event.latLng , isWindowOpened:false}]
+        }));
+    };
+
+
+    componentDidMount() {
+    }
+
+    render () {
+        return (
+            <div style={{height: '100%'}}>
+                <MapComponent
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    onMapClick={this.handleMapClick.bind(this)}
+                    markers = {this.state.markers}
+                />
+            </div>
+        )
+    }
+}
---
+++ src/components/MarkerInfoWindow.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import {Marker, InfoWindow} from "react-google-maps";
+
+export default class InfoWindowMap extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            isOpen: false,
+            isDetailOpen:false
+        }
+
+    }
+
+    handleToggleDetailOpen = () => {
+
+        this.setState({isDetailOpen: true});
+    };
+
+    handleClicks = (id) => {
+        console.log(id);
+        this.handleToggleDetailOpen();
+    };
+
+    handleToggleOpen = () => {
+
+        this.setState({isOpen: true});
+    };
+
+    handleMouseOver = (id) => {
+        console.log(id);
+        this.handleToggleOpen();
+    };
+
+    handleToggleClose = () => {
+
+        this.setState({isOpen: false});
+    }
+
+    handleMouseOut = (id) => {
+        console.log(id);
+        this.handleToggleClose();
+    };
+
+
+    render() {
+
+        return (
+
+            <Marker key={this.props.index} position={{
+                lat: this.props.lat,
+                lng: this.props.lng
+            }} label={this.props.index.toString()}
+                    onMouseOver={() => this.handleMouseOver(this.props.index)}
+                    onMouseOut={()=> this.handleMouseOut()}
+                    onClick={() => this.handleClicks(this.props.index)}
+            >
+
+                {
+                    this.state.isOpen &&
+                    <InfoWindow >
+                        <div>
+                            <p>Atrybuty domyślne</p>
+                            <p>Obrazek</p>
+                        </div>
+                    </InfoWindow>
+                }
+
+                {
+                    this.state.isDetailOpen &&
+                    <InfoWindow onCloseClick={() => this.setState({isDetailOpen: false})}>
+                        <div>
+                            <p>Atrybuty domyślne</p>
+                            <p>Reszta atrubutów</p>
+                            <p>Obrazek</p>
+                        </div>
+                    </InfoWindow>
+                }
+            </Marker>
+        )
+    }
+}
