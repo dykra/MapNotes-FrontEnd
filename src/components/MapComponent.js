032e48a4ff4bd45b6dce3945aa085aa97e17dd5a
dykra <asiapalewicz@gmail.com>
2018-04-28 22:24:54 +0200

    ODYS1-12 first version of search box

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,20 +1,95 @@
 import React from 'react';
-import { compose, withProps } from "recompose";
+import { compose, withProps, lifecycle } from "recompose";
 import { withGoogleMap, withScriptjs, GoogleMap } from 'react-google-maps';
 import InfoWindowMap from "./MarkerInfoWindow";
-
+const { SearchBox } = require("react-google-maps/lib/components/places/SearchBox");
+const _ = require("lodash");
 
 const Map = compose(
+    lifecycle({
+        componentWillMount() {
+            const refs = {}
+
+            this.setState({
+                bounds: null,
+                center: {
+                    lat: 41.9, lng: -87.624
+                },
+                markers: [],
+                onMapMounted: ref => {
+                    refs.map = ref;
+                },
+                onBoundsChanged: () => {
+                    this.setState({
+                        bounds: refs.map.getBounds(),
+                        center: refs.map.getCenter(),
+                    })
+                },
+                onSearchBoxMounted: ref => {
+                    refs.searchBox = ref;
+                },
+                onPlacesChanged: () => {
+                    const places = refs.searchBox.getPlaces();
+                    const bounds = new google.maps.LatLngBounds();
+
+                    places.forEach(place => {
+                        if (place.geometry.viewport) {
+                            bounds.union(place.geometry.viewport)
+                        } else {
+                            bounds.extend(place.geometry.location)
+                        }
+                    });
+                    const nextMarkers = places.map(place => ({
+                        position: place.geometry.location,
+                    }));
+                    const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+                    this.setState({
+                        center: nextCenter,
+                        markers: nextMarkers,
+                    });
+                    // refs.map.fitBounds(bounds);
+                },
+            })
+        },
+    }),
     withScriptjs,
     withGoogleMap,
 )
 (props => {
     return (
         <GoogleMap
+            ref={props.onMapMounted}
             defaultZoom={8}
             defaultCenter={{lat: -34.397, lng: 150.644}}
+            center={props.center}
             onClick={props.onMapClick}
+            onBoundsChanged={props.onBoundsChanged}
         >
+            <SearchBox
+                ref={props.onSearchBoxMounted}
+                bounds={props.bounds}
+                controlPosition={google.maps.ControlPosition.TOP_LEFT}
+                onPlacesChanged={props.onPlacesChanged}
+            >
+                <input
+                    type="text"
+                    placeholder="Customized your placeholder"
+                    style={{
+                        boxSizing: `border-box`,
+                        border: `1px solid transparent`,
+                        width: `240px`,
+                        height: `32px`,
+                        marginTop: `27px`,
+                        padding: `0 12px`,
+                        borderRadius: `3px`,
+                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                        fontSize: `14px`,
+                        outline: `none`,
+                        textOverflow: `ellipses`,
+                    }}
+                />
+            </SearchBox>
             {props.markers.map((marker, index) =>
                 <InfoWindowMap
                     lat={marker.position.lat()}
