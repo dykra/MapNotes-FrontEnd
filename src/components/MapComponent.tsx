a49de9c5991d6ecaf8f89ca2066ee414e5bfea8f
Malgorzata Stachona <malgorzata.stachon@ovoo.pl>
2018-05-07 19:48:22 +0200

    initial commit with side bar and transport input

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -6,6 +6,7 @@ import { WithScriptjsProps } from 'react-google-maps/lib/withScriptjs';
 import { WithGoogleMapProps } from 'react-google-maps/lib/withGoogleMap';
 import { GOOGLE_MAP_URL } from '../constants';
 import { MarkerData } from '../types/MarkerData';
+import { Button, ControlLabel, FormControl, Col, FormGroup } from 'react-bootstrap';
 
 interface MapProps {
     googleMapURL: String;
@@ -35,6 +36,8 @@ const Map = compose<MapProps, MapComposeProps>(
 
 interface MapContainerState {
     markers: MarkerData[];
+    visibleLeftBar: boolean;
+    transportInput: String;
 }
 
 export default class MapContainer extends React.Component<{}, MapContainerState> {
@@ -42,8 +45,14 @@ export default class MapContainer extends React.Component<{}, MapContainerState>
     constructor(props: {}) {
         super(props);
         this.handleMapClick = this.handleMapClick.bind(this);
+        this.showLeftBar = this.showLeftBar.bind(this);
+        this.hideLeftBar = this.hideLeftBar.bind(this);
+        this.searchForTransport = this.searchForTransport.bind(this);
+        this.onChangeDestinationInput = this.onChangeDestinationInput.bind(this);
         this.state = {
-            markers: []
+            markers: [],
+            visibleLeftBar: false,
+            transportInput: '',
         };
     }
 
@@ -51,6 +60,31 @@ export default class MapContainer extends React.Component<{}, MapContainerState>
         this.setState((prevState: any) => ({
             markers: [...prevState.markers, {position: event.latLng, isWindowOpened: false}]
         }));
+    }
+
+    showLeftBar(event: any) {
+        this.setState(
+            {
+                visibleLeftBar: true,
+            });
+    }
+
+    hideLeftBar(event: any) {
+        this.setState({
+            visibleLeftBar: false
+        });
+    }
+
+    onChangeDestinationInput(event: any) {
+        this.setState(({
+            transportInput: event.target.value
+        }));
+    }
+
+    searchForTransport(event: any) {
+        console.log('search transport');
+        console.log(event);
+        console.log(this.state.transportInput);
     }
 
     render() {
@@ -62,8 +96,67 @@ export default class MapContainer extends React.Component<{}, MapContainerState>
                 key={index}
             />)
         );
+        let leftBar;
+        if (this.state.visibleLeftBar) {
+            leftBar = (
+                <div className={'OpenedLeftBar'}>
+                    <div>
+                        <Button
+                            className={'CloseLeftBarButton'}
+                            onClick={this.hideLeftBar}
+                        >hide BAR
+                        </Button>
+                    </div>
+
+                    <div className={'TransportBar'}>
+                    <FormGroup>
+                        <Col componentClass={ControlLabel} sm={8}>Select Destiantion</Col>
+                        <Col sm={8}>
+                            <FormControl
+                                placeholder="enter destination"
+                                onChange={this.onChangeDestinationInput}
+                            />
+                        </Col>
+                        <Button
+                            className={'SearchTransportButton'}
+                            bsSize="small"
+                            active={true}
+                            onClick={this.searchForTransport}
+                        >SEARCH
+                        </Button>
+                    </FormGroup>
+                    </div>
+
+                    {/*<div className={'PinBar'}>PIN elements</div>*/}
+                    {/*<div className={'TransportBar'}>*/}
+                        {/*<div>Select destination</div>*/}
+                            {/*<input*/}
+                                {/*placeholder="destination"*/}
+                                {/*onChange={this.onChangeDestinationInput}*/}
+                            {/*/>*/}
+                        {/*<button*/}
+                            {/*className={'SearchTransportButton'}*/}
+                            {/*onChange={this.searchForTransport}*/}
+                        {/*> search*/}
+                        {/*</button>*/}
+                    {/*</div>*/}
+                </div>
+            );
+        } else {
+            leftBar = (
+                <div className={'ClosedLeftBar'}>
+                    <button
+                        className={'OpenLeftBarButton'}
+                        onClick={this.showLeftBar}
+                    > open BAR
+                    </button>
+                </div>
+            );
+        }
+
         return (
             <div style={{height: '100%'}}>
+                {leftBar}
                 <Map
                     googleMapURL={GOOGLE_MAP_URL}
                     loadingElement={<div style={{height: `100%`}}/>}
