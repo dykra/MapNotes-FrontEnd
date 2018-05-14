3efd2f9943cc1cd6e85ef5443baca91141722250
Anna Bukowska <woda33@vp.pl>
2018-05-15 01:50:07 +0200

    [Oddys1-15] Start

--- Utils/parseFilterInput.ts
+++ Utils/parseFilterInput.ts
@@ -4,16 +4,55 @@ function conditionsToArray (input: string, separator: string): Array<string> {
     return input.split(separator, MAX_CONDITIONS - 1)
 }
 
+function parseWithAttributeCondition (condition: string): Array<string> {
+    const parsed = conditionsToArray(condition, ":")
+    if (parsed.length != 2) {
+        //błąd zagnieżdżonego atrybutu?
+    }
+    const attribute: Array<string> = parseParameter(parsed[0])
+    const value: Array<string> = parseValue(parsed[1])
+}
 
+function parseParameter (parameter: string): Array<string> {
+    if (condition.includes("<") || condition.includes(">") || condition.includes("=")) {
+        //błąd znak mniejszy/większy/rowny bez atrybutu
+    } else if (condition.includes(".")) {
+        return parseExactlyCondition(condition.trim())
+    } else {
+        return parsePieceCondition(condition.trim())
+    }
+}
+
+
+// reserved: ":" "|" "&" "."
+function parseCondition (condition: string): Array<string> {
+    if (condition.includes(":")) {
+        return parseWithAttributeCondition(condition)
+    } else {
+        return parseParameter(condition)
+    }
+}
 
 function parseFilterInput (input: string): Array<any> {
 
     const orConditions: Array<string> = conditionsToArray(input, "|");
 
-    let conditions: Array<Array<string>> = [[]];
+    let conditions: Array<Array<string>> = [];
     for(let i = 0; i < orConditions.length; i++) {
-        conditions.push(conditionsToArray(input, "&"))
+        conditions.push(conditionsToArray(orConditions[i], "&"))
     }
 
-    return conditions //wrong!!
+    let filter: Array<Array<Array<string>>> = [];
+    for(let i = 0; i < conditions.length; i++) {
+        filter.push([]);
+        for(let j = 0;j < conditions[i].length; j++) {
+            try {
+                filter[i].push(parseCondition(conditions[i][j]))
+            } catch (exception) {
+                //wrong input
+            }
+        }
+    }
+
+    return filter
 }
