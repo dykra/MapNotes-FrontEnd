055cf1d16ac862a8c6a80d886ca11614e694f35b
imthumbelina <aga.zmc@gmail.com>
2018-05-21 21:11:46 +0200

    stashing

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -114,7 +114,7 @@ export default class MapContainer extends React.Component<{mapId: any}, MapConta
         var marker: MarkerData = {
             position: new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()),
             isWindowOpened: false,
-            groupName: "red",
+            groupName: 'red',
         };
 
         var pin1: PinData = {
