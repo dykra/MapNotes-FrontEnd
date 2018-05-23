2fd89fe091fc377cfdcc4b840d45bcbe42896335
justynamaciag <justynamaciag96@gmail.com>
2018-05-23 07:43:55 +0200

    One test added

--- src/spec/App.test.tsx
+++ src/spec/App.test.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../components/App';
+import MapMenu from "../components/MapMenu";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('create map', () => {
+    const div = document.createElement('div');
+    ReactDOM.render( <MapMenu />, div);
 });
---
+++ src/tests/AppTest.tsx
@@ -0,0 +1,11 @@
+const React = require('react');
+const e = React.createElement;
+const ReactDom = require('react-dom');
+const App = require('../components/App');
+
+describe('app component', function () {
+   it('should work', function () {
+     ReactDOM.render(e(App), document.getElementById('container'));
+
+   })
+});
