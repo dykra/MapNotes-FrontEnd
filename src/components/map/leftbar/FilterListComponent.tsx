018b0c46e655e8c63947a23ce066337e4c4723b2
justynamaciag <justynamaciag96@gmail.com>
2018-06-08 11:44:32 +0200

    Printing note under button changed

--- src/components/map/leftbar/FilterListComponent.tsx
+++ src/components/map/leftbar/FilterListComponent.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { PinData } from '../../../types/api/PinData';
 import '../../../styles/map/GroupsStyle.css';
+import { getPinById } from '../../../api/PinApi';
+import { PinAttr } from '../../../types/creation/PinAttr';
 
 export interface FilterListComponentProps {
     visiblePins: PinData[];
@@ -10,25 +12,58 @@ export interface FilterListComponentProps {
 
 export interface FilterListComponentState {
     pins: String[];
+    pinIdPrinted: number;
+    buttonClicked: boolean;
+    attributes: PinAttr[];
 }
 
-export class FilterListComponent extends React.Component<FilterListComponentProps , FilterListComponentState> {
-    constructor(props: FilterListComponentProps ) {
+export class FilterListComponent extends React.Component<FilterListComponentProps, FilterListComponentState> {
+    constructor(props: FilterListComponentProps) {
         super(props);
 
         this.state = {
             pins: ['pin1', 'pin2', 'pin3'],
+            pinIdPrinted: 0, // TODO - co tu wpisac??
+            buttonClicked: false,
+            attributes: [],
         };
+
+        this.getPinCallback = this.getPinCallback.bind(this);
 
     }
 
-    handleClick(id: Number) {
-        console.log('button was clicked' + id);
+    handleClick(id: number) {
+        if (!this.state.buttonClicked) {
+            this.setState({buttonClicked: true});
+            this.setState({pinIdPrinted: id});
 
+        } else {
+            this.setState({buttonClicked: false});
+        }
+    }
+
+    getPinCallback(pin: PinData) {
+        this.setState({attributes: pin.data.attributes});
+    }
+
+    renderNote(id: number) {
+        if (this.state.buttonClicked && id === this.state.pinIdPrinted) {
+            getPinById(this.state.pinIdPrinted, this.getPinCallback);
+            return this.state.attributes.map(e =>
+                (
+                    <div key={e.name}>
+                        <b>
+                            {e.name}
+                        </b> {e.value}
+                    </div>
+                )
+            );
+        }
+        return null;
     }
 
     renderList() {
-        return(
+        return (
             <ul>
                 {this.state.pins.map((pins, i) => {
                         const id = i;
@@ -37,7 +72,10 @@ export class FilterListComponent extends React.Component<FilterListComponentProp
                                 <Button onClick={() => this.handleClick(i)}>
                                     Pin {id}
                                 </Button>
-                            </li>);
+                                {this.renderNote(id)}
+                            </li>
+
+                        );
                     }
                 )}
             </ul>
@@ -48,6 +86,6 @@ export class FilterListComponent extends React.Component<FilterListComponentProp
         return (
             <div className="filterList">
                 {this.renderList()}
-        </div>);
+            </div>);
     }
 }
