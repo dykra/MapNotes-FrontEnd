998cd65f2822658d3d1329cd1b561b290f0588c7
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-06-09 00:36:01 +0200

    api error handling

--- src/api/PinApi.ts
+++ src/api/PinApi.ts
@@ -4,32 +4,44 @@ import { PinData } from '../types/api/PinData';
 
 const PIN_URL: string = API_BASE_URL + '/pin';
 
-export function deletePin(id: number, cb: () => void) {
+export function deletePin(id: number, cb: () => void, er?: (error: any) => void) {
     axios.delete(PIN_URL + '/' + id)
         .then(function () {
             cb();
         })
         .catch(function (error: any) {
-            console.log(error);
+            if (er) {
+                er(error);
+            } else {
+                console.log(error);
+            }
         });
 }
 
-export function getAllPins(cb: (pins: PinData[]) => void) {
+export function getAllPins(cb: (pins: PinData[]) => void, er?: (error: any) => void) {
     axios.get(PIN_URL)
         .then(function (response: any) {
             cb(response.data);
         })
         .catch(function (error: any) {
-            console.log(error);
+            if (er) {
+                er(error);
+            } else {
+                console.log(error);
+            }
         });
 }
 
-export function getPinById(id: number, cb: (pin: PinData) => void) {
+export function getPinById(id: number, cb: (pin: PinData) => void, er?: (error: any) => void) {
     axios.get(PIN_URL + '/' + id)
         .then(function (response: any) {
             cb(response.data);
         })
         .catch(function (error: any) {
-            console.log(error);
+            if (er) {
+                er(error);
+            } else {
+                console.log(error);
+            }
         });
 }
