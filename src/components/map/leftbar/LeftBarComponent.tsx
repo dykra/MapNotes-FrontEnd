a16b497c6c5c5b418fe75f7ff87f11956978e55b
Radomir Krawczykiewicz <kotwic4@gmail.com>
2018-06-10 17:31:55 +0200

    [ODYS1-24] Sidebar new look

--- src/components/map/leftbar/LeftBarComponent.tsx
+++ src/components/map/leftbar/LeftBarComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Panel } from 'react-bootstrap';
 import { TransportComponent } from './TransportComponent';
 import { GroupsComponent } from './GroupsComponent';
 
@@ -7,6 +7,8 @@ import FilterComponent from './FilterComponent';
 import { Filter } from '../../../types/filter/Filter';
 import { PinData } from '../../../types/api/PinData';
 import { Link } from 'react-router-dom';
+import * as Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import '../../../styles/map/leftbar/LeftBarComponent.css';
 
 export interface LeftBarComponentProps {
     filter: (filter: Filter) => void;
@@ -59,7 +61,7 @@ export class LeftBarComponent extends React.Component<LeftBarComponentProps, Lef
         });
     }
 
-    render() {
+    renderBody() {
         if (this.props.leftBarComponentChild) {
             return (
                 <div className="OpenedLeftBar">
@@ -70,54 +72,100 @@ export class LeftBarComponent extends React.Component<LeftBarComponentProps, Lef
         if (this.state.visibleLeftBar) {
            return (
                <div className="OpenedLeftBar">
-                   <div>
-                       <Button
-                           className="CloseLeftBarButton btn btn-primary"
-                           onClick={this.hideLeftBar}
-                       >
-                           hide BAR
-                       </Button>
-                       <Button
-                           className="DeleteMapButton btn btn-danger"
-                           onClick={() => {
-                               if (window.confirm('Are you sure you wish to delete map?')) {
-                                   this.props.deleteMap();
-                               }
-                           }}
-                       >
-                           Delete Map
-                       </Button>
-                       <Link to="/home">
+                   <div className="LeftBarContent" >
+                       <div className="NavButtons Buttons">
                            <Button
-                               className="HomeButton"
+                               className="DeleteMapButton btn btn-danger"
+                               onClick={() => {
+                                   if (window.confirm('Are you sure you wish to delete map?')) {
+                                       this.props.deleteMap();
+                                   }
+                               }}
                            >
-                               Home
+                               Delete Map
                            </Button>
-                       </Link>
+                           <Link to="/home">
+                               <Button
+                                   className="btn btn-success"
+                               >
+                                   Home
+                               </Button>
+                           </Link>
+                           <Button
+                               className="CloseLeftBarButton btn btn-primary"
+                               onClick={this.hideLeftBar}
+                           >
+                               <Glyphicon glyph="remove"/>
+                           </Button>
+                       </div>
+                       <Panel id="Transport" defaultExpanded={true}>
+                           <Panel.Heading>
+                               <Panel.Title toggle={true}>
+                                   Transport
+                               </Panel.Title>
+                           </Panel.Heading>
+                           <Panel.Collapse>
+                               <Panel.Body>
+                                   <TransportComponent
+                                       onRef={(ref: any) => (this.references.transportComponent = ref)}
+                                       showRoadBetweenMarkers={this.props.showRoadBetweenMarkers}
+                                       visiblePins={this.props.visiblePins}
+                                   />
+                               </Panel.Body>
+                           </Panel.Collapse>
+                       </Panel>
+                       <Panel id="Filter" defaultExpanded={true}>
+                           <Panel.Heading>
+                               <Panel.Title toggle={true}>
+                                   Filter
+                               </Panel.Title>
+                           </Panel.Heading>
+                           <Panel.Collapse>
+                               <Panel.Body>
+                                   <FilterComponent
+                                       filter={this.props.filter}
+                                       removeFilter={this.props.removeFilter}
+                                   />
+                               </Panel.Body>
+                           </Panel.Collapse>
+                       </Panel>
+                       <Panel id="Group" defaultExpanded={true}>
+                           <Panel.Heading>
+                               <Panel.Title toggle={true}>
+                                   Group
+                               </Panel.Title>
+                           </Panel.Heading>
+                           <Panel.Collapse>
+                               <Panel.Body>
+                                   <GroupsComponent
+                                       changePins={this.props.changePins}
+                                       visiblePins={this.props.visiblePins}
+                                   />
+                               </Panel.Body>
+                           </Panel.Collapse>
+                       </Panel>
                    </div>
-                   <TransportComponent
-                       onRef={(ref: any) => (this.references.transportComponent = ref)}
-                       showRoadBetweenMarkers={this.props.showRoadBetweenMarkers}
-                       visiblePins={this.props.visiblePins}
-                   />
-                   <FilterComponent
-                       filter={this.props.filter}
-                       removeFilter={this.props.removeFilter}
-                   />
-                   <GroupsComponent
-                       changePins={this.props.changePins}
-                       visiblePins={this.props.visiblePins}
-                   />
                </div>
             );
         }
         return (
             <div className="ClosedLeftBar">
-                <button
-                    className="OpenLeftBarButton btn btn-primary"
-                    onClick={this.showLeftBar}
-                > open BAR
-                </button>
+                <div className="LeftBarContent" >
+                    <Button
+                        className="btn btn-primary"
+                        onClick={this.showLeftBar}
+                    >
+                        <Glyphicon glyph="menu-hamburger"/>
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        return(
+            <div className="LeftBar">
+                {this.renderBody()}
             </div>
         );
     }
