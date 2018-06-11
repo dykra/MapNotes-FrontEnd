97d0ad8dc12fce44707c8db578d49d955128d373
dykra <asiapalewicz@gmail.com>
2018-06-11 21:47:03 +0200

    ODYS1-28 add distance

--- src/components/map/leftbar/TransportComponent.tsx
+++ src/components/map/leftbar/TransportComponent.tsx
@@ -123,7 +123,11 @@ export class TransportComponent extends React.Component<TransportComponentProps,
                         REMOVE PATH
                     </Button>
                 </div>
+                <label>
+                    Car distance: {this.state.currentDistance} km
+                </label>
             </div>
+
         );
     }
 }
